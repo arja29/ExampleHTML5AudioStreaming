988594ef9f88098813c4068842bb4d1bbb38a8b5
arja29 <ardpy@outlook.com>
2014-03-01 07:58:19 -0800

    Update html5audio.js

--- www/scripts/html5audio.js
+++ www/scripts/html5audio.js
@@ -1,4 +1,3 @@
-
 var progressTimer;
 
 var playButton;
@@ -21,7 +20,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://64.120.228.226:8058/listen.pls;';
+var myaudioURL = 'http://64.120.228.226:8058/;';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
